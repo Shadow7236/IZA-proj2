73a9109c025a6cdf0c13155513b56ee10953062e
Radovan Klembara <radovan@klembara.pro>
2020-05-12 11:19:10 +0200

    initial work

---
+++ HungryStudentTests/HungryStudentTests.swift
@@ -0,0 +1,34 @@
+//
+//  HungryStudentTests.swift
+//  HungryStudentTests
+//
+//  Created by Radovan Klembara on 12/05/2020.
+//  Copyright © 2020 Radovan Klembara. All rights reserved.
+//
+
+import XCTest
+@testable import HungryStudent
+
+class HungryStudentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
